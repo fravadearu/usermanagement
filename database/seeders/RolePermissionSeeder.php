d3ca2992f69610db0d2dfaeebb295e6925f72391
fravadearu <fravadearu@gmail.com>
2024-12-03 10:50:06 +0000

    new file, mysql on nix

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    
+public function run(): void
+{
+    $permissions = [
+        'tambah-user',
+        'lihat-user',
+        'edit-user',
+        'hapus-user',
+        'tambah-tulisan',
+        'lihat-tulisan',
+        'edit-tulisan',
+        'hapus-tulisan',
+    ];
+    foreach ($permissions as $permission) {
+        Permission::create(['name' => $permission]);
+    }
+
+    // Create roles
+    $roles = [
+        'admin',
+        'penulis',
+    ];
+    foreach ($roles as $role) {
+        Role::create(['name' => $role]);
+    }
+
+    // Assign permissions to roles
+    $roleAdmin = Role::findByName('admin');
+    $roleAdmin->givePermissionTo(['tambah-user', 'lihat-user', 'edit-user', 'hapus-user']); 
+
+    $rolePenulis = Role::findByName('penulis');
+    $rolePenulis->givePermissionTo(['tambah-tulisan', 'lihat-tulisan', 'edit-tulisan', 'hapus-tulisan']); 
+}
+
+}
