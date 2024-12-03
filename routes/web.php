41050c0a085d34dbab00aba83725ddb41ddf9ad3
fravadearu <fravadearu@gmail.com>
2024-12-03 11:28:00 +0000

    add routes

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::get('/admin1', function(){
+    return '<h1>HElO Admin</h1>';
+})->middleware(['auth','verified']);
+
 require __DIR__.'/auth.php';
